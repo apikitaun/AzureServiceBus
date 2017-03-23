c1ae0a1e947197b8d792e3cd40fdec511761e81a
jorge <jorge.gcia.gcia@gmail.com>
2017-03-23 09:41:48 +0100

    Agregar archivos de proyecto.

---
+++ TestServiceBus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace TestServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Endpoint=sb://xxxservicebus1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=p/bAbB/B1UzskSpeVeEsmwjH+TS/Fs2kGgyFlmcomxA=";
+            var queueName = "Queue1";
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            BrokeredMessage message = new BrokeredMessage("This is one message from Sdt Service Bus client ["+Guid.NewGuid()+"]");
+            client.Send(message);
+            Console.WriteLine("Message Sent");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestServiceBusReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace TestServiceBusReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Endpoint=sb://xxxservicebus1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=p/bAbB/B1UzskSpeVeEsmwjH+TS/Fs2kGgyFlmcomxA=";
+            var queueName = "Queue1";
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
